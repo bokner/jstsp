0e1d52fa11327ae4a880347ddd0914d70575f558
Boris Okner <boris.okner@gmail.com>
2021-12-17 09:17:57 -0500

    Add test for the solution to Y/S sample obtained by our model (yuck)

--- test/jstsp_test.exs
+++ test/jstsp_test.exs
@@ -11,21 +11,12 @@ defmodule JstspTest do
     data_instance = Path.join([:code.priv_dir(:jstsp), "mzn", "da_silva1.dzn"])
 
     {:ok, res} =
-    MinizincSolver.solve_sync(model, data_instance,
+      MinizincSolver.solve_sync(model, data_instance,
         solver: "gecode",
         solution_handler: JSTSP.MinizincHandler
       )
 
-    model_results =
-      res
-      |> MinizincResults.get_last_solution()
-      |> then(fn solution ->
-        %{
-          schedule: MinizincResults.get_solution_value(solution, "schedule"),
-          magazine: MinizincResults.get_solution_value(solution, "magazine"),
-          objective: MinizincResults.get_solution_objective(solution)
-        }
-      end)
+    model_results = model_results(res)
 
     switches = count_switches(model_results.schedule, model_results.magazine)
     assert switches == model_results.objective
@@ -33,12 +24,34 @@ defmodule JstspTest do
 
   test "Yanasse, Senne" do
     sample = jstsp_set_sample()
+    our_schedule = [2, 9, 10, 1, 11, 14, 8, 4, 6, 12, 15, 13, 7, 3, 5]
+    solution_constraint = {:model_text, "constraint schedule = #{inspect(our_schedule)};"}
     m = Enum.max(List.flatten(sample.jobs))
-    job_tools = Enum.map(sample.jobs,
-      fn job -> Enum.map(1..m, fn i -> (i in job) && 1 || 0 end) end)
+
+    job_tools =
+      Enum.map(
+        sample.jobs,
+        fn job -> Enum.map(1..m, fn i -> (i in job && 1) || 0 end) end
+      )
+
     data = %{C: 10, J: 15, T: 15, job_tools: job_tools}
     model = Path.join([:code.priv_dir(:jstsp), "mzn", "jstsp.mzn"])
-    MinizincSolver.solve_sync(model, data, solver: "yuck", solution_handler: JSTSP.MinizincHandler, time_limit: 15_000)
+
+    {:ok, mzn_results} =
+      MinizincSolver.solve_sync([model, solution_constraint], data,
+        solver: "cplex",
+        solution_handler: JSTSP.MinizincHandler,
+        time_limit: 150_000
+      )
+
+    model_results = model_results(mzn_results)
+
+    switches = count_switches(model_results.schedule, model_results.magazine)
+    assert switches == model_results.objective
+    ## The optimal value claimed by Yanasse, Senne
+    assert model_results.objective == 13
+    ## ... is obtained by our model with different schedule
+    assert model_results.schedule == our_schedule
   end
 
   defp jstsp_sample() do
@@ -75,7 +88,20 @@ defmodule JstspTest do
         [1, 2, 5, 8, 9, 10, 12, 14, 15],
         [3, 4, 5, 6, 8, 9, 11, 12, 13, 14],
         [1, 2, 4, 7, 9, 10, 12, 13, 14, 15]
-      ]
+      ],
+      schedule: [3, 12, 6, 11, 1, 8, 4, 14, 5, 9, 2, 10, 7, 13, 15]
     }
+  end
+
+  defp model_results(mzn_results) do
+    mzn_results
+    |> MinizincResults.get_last_solution()
+    |> then(fn solution ->
+      %{
+        schedule: MinizincResults.get_solution_value(solution, "schedule"),
+        magazine: MinizincResults.get_solution_value(solution, "magazine"),
+        objective: MinizincResults.get_solution_objective(solution)
+      }
+    end)
   end
 end
