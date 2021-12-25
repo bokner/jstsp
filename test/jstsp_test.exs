75d220af11f2882071612fb7b4f11d99a149820d
Boris Okner <boris.okner@gmail.com>
2021-12-24 20:49:17 -0500

    Patch the Y/S solution with dominated jobs

--- test/jstsp_test.exs
+++ test/jstsp_test.exs
@@ -52,32 +52,32 @@ defmodule JstspTest do
     assert model_results.schedule == our_schedule
   end
 
-  # test "Yanasse, Senne (2)" do
-  #   sample = jstsp_set_sample2()
-  #   our_schedule = [11, 15, 6, 14, 18, 1, 12, 9, 3, 20, 5, 17, 2, 10, 4, 7, 8, 16, 13, 19]
-  #   job_tools = to_matrix(sample.jobs)
+   test "Yanasse, Senne (2)" do
+    sample = jstsp_set_sample2()
+    our_schedule = [11, 15, 6, 14, 18, 1, 12, 9, 3, 20, 5, 17, 2, 10, 4, 7, 8, 16, 13, 19]
+    job_tools = to_matrix(sample.jobs)
 
-  #   data =
-  #     sample
-  #     |> Map.take([:C, :T, :J])
-  #     |> Map.put(:job_tools, job_tools)
+    data =
+      sample
+      |> Map.take([:C, :T, :J])
+      |> Map.put(:job_tools, job_tools)
 
-  #   solution_constraint = {:model_text, upper_bound_constraint(25)}
-  #   model = Path.join([:code.priv_dir(:jstsp), "mzn", "jstsp.mzn"])
-  #   {:ok, mzn_results} =
-  #     MinizincSolver.solve_sync([model, solution_constraint], data,
-  #       solver: "cplex",
-  #       solution_handler: JSTSP.MinizincHandler,
-  #       time_limit: 1200_000
-  #     )
+    solution_constraint = {:model_text, schedule_constraint(sample.schedule)}
+    model = Path.join([:code.priv_dir(:jstsp), "mzn", "jstsp.mzn"])
+    {:ok, mzn_results} =
+      MinizincSolver.solve_sync([model, solution_constraint], data,
+        solver: "cplex",
+        solution_handler: JSTSP.MinizincHandler,
+        time_limit: 1200_000
+      )
 
-  #   model_results = model_results(mzn_results)
+    model_results = model_results(mzn_results)
 
-  #   switches = count_switches(model_results.schedule, model_results.magazine)
-  #   assert switches == model_results.objective
-  #   ## The optimal value claimed by Yanasse, Senne
-  #   assert model_results.objective == 13
-  # end
+    switches = count_switches(model_results.schedule, model_results.magazine)
+    assert switches == model_results.objective
+    ## The optimal value claimed by Yanasse, Senne
+    assert model_results.objective == 13
+  end
 
   defp jstsp_sample() do
     %{
@@ -149,23 +149,23 @@ defmodule JstspTest do
   # T25 = {1,2,4,7,9,10,12,13,14,15}
   def jstsp_set_sample2() do
     jobs = [
-      # [1, 2],
-      # [5, 15],
-      # [4, 7, 11],
-      # [2, 8, 9],
-      # [5, 13, 15],
-      # [1, 7, 9, 13],
+      [1, 2],
+      [5, 15],
+      [4, 7, 11],
+      [2, 8, 9],
+      [5, 13, 15],
+      [1, 7, 9, 13],
       [3, 7, 10, 11],
-      # [4, 5, 7, 8, 9],
-      # [2, 7, 9, 10, 15],
+      [4, 5, 7, 8, 9],
+      [2, 7, 9, 10, 15],
       [1, 3, 5, 8, 11],
       [2, 5, 9, 11, 14],
       [7, 8, 9, 11, 13, 14],
       [1, 4, 5, 11, 12, 14],
       [2, 3, 5, 7, 11, 13],
-      # [1, 4, 6, 8, 9, 10],
+      [1, 4, 6, 8, 9, 10],
       [1, 2, 5, 8, 10, 11, 12],
-      # [3, 4, 5, 6, 8, 9, 11],
+      [3, 4, 5, 6, 8, 9, 11],
       [4, 7, 8, 11, 12, 13, 14],
       [1, 3, 5, 6, 7, 9, 11, 15],
       [1, 4, 5, 6, 7, 8, 9, 10],
@@ -180,8 +180,24 @@ defmodule JstspTest do
       J: length(jobs),
       T: 15,
       C: 10,
-      jobs: jobs
-    }
+      jobs: jobs,
+      schedule: Enum.reverse([11, 22,
+      5, 2, ## dominated jobs
+      14, 21,
+      8, ## dominated
+      7, 18,
+      3, ## dominated
+      12, 24,
+      17, ## dominated
+      13, 19, 10, 20,
+      15, ## dominated
+      16,
+      1, ## dominated
+      23,
+      4, ## dominated,
+      25,
+      6, 9 ## dominated
+      ])}
   end
 
   defp model_results(mzn_results) do
