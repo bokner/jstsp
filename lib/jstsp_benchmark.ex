381749f3b9d57bd9bad3e61ec8242b27aa7e15d1
Boris Okner <boris.okner@gmail.com>
2022-01-29 15:31:45 -0500

    Benchmark: minor

--- lib/jstsp_benchmark.ex
+++ lib/jstsp_benchmark.ex
@@ -1,9 +1,22 @@
 defmodule JSTSP.Benchmark do
   import JSTSP.Utils
-  def benchmark15x15(opts \\ default_solver_opts()) do
-    instance = "instances/MTSP/Laporte/Tabela5/L1-2.txt"
+
+  def benchmark(instance, opts \\ default_solver_opts())
+
+  def benchmark(instance, opts) when is_binary(instance) do
     instance
     |> get_instance_data()
-    |> JSTSP.run_model(opts)
+    |> benchmark(opts)
+  end
+
+  def benchmark(instance, opts) when is_map(instance) do
+    JSTSP.run_model(instance, opts)
+  end
+  def benchmark15x15(opts) do
+    benchmark("instances/MTSP/Laporte/Tabela5/L1-2.txt", opts)
+  end
+
+  def benchmark25x25(opts) do
+    benchmark("instances/MTSP/Laporte/Tabela6/L22-3.txt", opts)
   end
 end
